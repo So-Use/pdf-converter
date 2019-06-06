52e4f8d25310a4225fce2ba90f0831e9e79dd364
Sébastien Prunier <sebastien.prunier@serli.com>
2019-06-06 11:57:07 +0200

    Manage Otoroshi exchenge protocol

--- src/server.js
+++ src/server.js
@@ -7,13 +7,21 @@ const pdf = require('./pdf.js');
 
 const jsonParser = bodyParser.json()
 
-app.use(function(req, res, next) {
-  const otoroshiState = req.header("Otoroshi-State")
-  if (otoroshiState) {
-    res.header("Otoroshi-State-Resp", otoroshiState)
-  }
-  next();
-});
+const claimSharedkey = process.env.CLAIM_SHAREDKEY;
+const otoroshiExchangeProtocolEnabled = claimSharedkey && claimSharedkey !== "";
+if (otoroshiExchangeProtocolEnabled) {
+  console.log("Otoroshi exchange protocol is enabled");
+  app.use(function(req, res, next) {
+    const otoroshiState = req.header("Otoroshi-State")
+    if (otoroshiState) {
+      res.header("Otoroshi-State-Resp", otoroshiState)
+      next();
+    } else {
+      res.status(400).send("Bad Request");
+    }
+    
+  });
+}
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
