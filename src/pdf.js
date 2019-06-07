f10bbe3962429434e0e83719c2c7aee29bcb1a23
Sébastien Prunier <sebastien.prunier@serli.com>
2019-06-07 15:09:39 +0200

    Manage logs and errors

--- src/pdf.js
+++ src/pdf.js
@@ -2,22 +2,25 @@ const fs = require('fs')
 const path = require('path')
 const RenderPDF = require('chrome-headless-render-pdf')
 
+const { deleteSilently } = require('./file-utils')
+
 const generatePDF = (inputAsHTML, name, callback) => {
   const fileName = new Date().getTime()
   const pathFileName = path.resolve(__dirname, `../inputs/html/${name}-${fileName}.html`)
   const outputPathFileName = path.resolve(__dirname, `../outputs/pdf/${name}-${fileName}.pdf`)
+
   fs.writeFile(pathFileName, inputAsHTML, (err) => {
     if (err) {
-      console.log(err)
+      callback(err, null)
     } else {
-      RenderPDF.generateSinglePdf(`file://${pathFileName}`, outputPathFileName, {}).then(() => {
-        fs.unlink(pathFileName, (err) => {
-          if (err) {
-            console.log(err)
-          }
+      RenderPDF.generateSinglePdf(`file://${pathFileName}`, outputPathFileName, {})
+        .then(() => {
+          deleteSilently(pathFileName)
+          callback(null, outputPathFileName)
         })
-        callback(outputPathFileName)
-      })
+        .catch((err) => {
+          callback(err, null)
+        })
     }
   })
 }
--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const fs = require('fs')
 
 const pdf = require('./pdf.js')
+const { deleteSilently } = require('./file-utils')
 
 const jsonParser = bodyParser.json()
 
 const claimSharedkey = process.env.CLAIM_SHAREDKEY
 const otoroshiExchangeProtocolEnabled = claimSharedkey && claimSharedkey !== ''
 if (otoroshiExchangeProtocolEnabled) {
-  console.log('Otoroshi exchange protocol is enabled')
+  console.info('Otoroshi exchange protocol is enabled')
   app.use((req, res, next) => {
     const otoroshiState = req.header('Otoroshi-State')
     if (otoroshiState) {
@@ -35,20 +35,24 @@ app.get('/ping', (req, res) => {
 app.post('/convert/pdf/', jsonParser, (req, res) => {
   const content = req.body.content
   const name = req.body.name
-  pdf.generatePDF(content, name, (outputFileName) => {
-    console.log(outputFileName + ' generated')
-    const headers = {
-      'headers': {
-        'Content-Type': 'application/pdf'
-      }
-    }
-    res.sendFile(outputFileName, headers, () => {
-      fs.unlink(outputFileName, (err) => {
-        if (err) {
-          console.log(err)
+  console.info(`Starting PDF generation for ${name}`)
+  console.time('pdf-generation')
+  pdf.generatePDF(content, name, (err, outputFileName) => {
+    if (err) {
+      console.error(err)
+      res.status(500).send('Internal Server Error')
+    } else {
+      console.info(`${name} generated!`)
+      console.timeEnd('pdf-generation')
+      const headers = {
+        'headers': {
+          'Content-Type': 'application/pdf'
         }
+      }
+      res.sendFile(outputFileName, headers, () => {
+        deleteSilently(outputFileName)
       })
-    })
+    }
   })
 })
 
